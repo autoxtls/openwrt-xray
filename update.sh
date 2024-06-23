76ef7e7f1e02847f01af5567edb934778e587feb
Auto XTLS <148060975+autoxtls@users.noreply.github.com>
2024-06-24 00:56:20 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-version=$1
+version=$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases/latest | jq -r '.tag_name' | sed 's/v//g')
 sha=$(curl "https://codeload.github.com/XTLS/xray-core/tar.gz/v{$version}" | sha256sum - | awk '{print $1'})
 sed "s/^PKG_VERSION.*/PKG_VERSION:=${version}/g" Makefile > Makefile1
 sed "s/^PKG_HASH.*/PKG_HASH:=${sha}/g" Makefile1 > Makefile2
